fc29b0c6bdb640042e752c67db61fcf2c80dac00
Martin D <martin.deyres@gmail.com>
2024-07-08 14:58:18 +0200

    ajout crawler

--- formation/formation/spiders/simplon.py
+++ formation/formation/spiders/simplon.py
@@ -1,10 +1,45 @@
 import scrapy
+from ..items import FormationItem
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
-
-class SimplonSpider(scrapy.Spider):
+class SimplonSpider(CrawlSpider):
     name = "simplon"
     allowed_domains = ["simplon.co"]
-    start_urls = ["https://simplon.co"]
+    start_urls = ["https://simplon.co/notre-offre-de-formation.html"]
 
-    def parse(self, response):
-        pass
+    rules = [Rule(LinkExtractor(restrict_xpaths='//a[contains(text(),"Découvrez la formation")]'), callback='parse_item', follow=False),]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, meta={"playwright": True}) #playwright permet d'avoir du javascript fonctionnel quand le scraper visite la page
+
+    def parse_item(self, response):
+
+        item = FormationItem()
+        item['title']= response.xpath('//h1/text()').get()
+        # item['rncp']= response.xpath
+        yield item
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
