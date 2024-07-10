850073154d16a2026f36f61e4e8a5e0b9d48ea34
StevenDelval <asteven59220@free.fr>
2024-07-10 10:28:28 +0200

    add xpath

--- formation/formation/spiders/francecompetences.py
+++ formation/formation/spiders/francecompetences.py
@@ -4,7 +4,7 @@ from ..items import FranceCompetencesItem
 class FrancecompetencesSpider(scrapy.Spider):
     name = "francecompetences"
     allowed_domains = ["francecompetences.fr"]
-    start_urls = ["https://www.francecompetences.fr/recherche/rncp/37682/","https://www.francecompetences.fr/recherche/rs/5487/","https://www.francecompetences.fr/recherche/rncp/37827/"]
+    start_urls = ["https://www.francecompetences.fr/recherche/rncp/37682/","https://www.francecompetences.fr/recherche/rncp/34757/","https://www.francecompetences.fr/recherche/rs/5487/","https://www.francecompetences.fr/recherche/rncp/37827/"]
 
     def parse(self, response):
         item = FranceCompetencesItem()
@@ -22,6 +22,10 @@ class FrancecompetencesSpider(scrapy.Spider):
                 certificateur.append(cell_text)
             
             item["certificateur"].append(certificateur)
+        item["est_actif"] = response.xpath('//div[@class="banner--fcpt-certification__body__tags"]/p[2]/span[2]/text()').get()
+        item["niveau_de_qualification"] = response.xpath('//div[@class="list--fcpt-certification--essential--desktop__line"]/p[contains(normalize-space(), "niveau de qualification")]/../div/p/span/text()').get()
+        item["date_echeance_enregistrement"] = response.xpath('//div[@class="list--fcpt-certification--essential--desktop__line"]/p[contains(normalize-space(), "Date d’échéance")]/../div/p/span/text()').get()
+        item["title"] = response.xpath('//h1/text()').get()
         
         
         yield item   
