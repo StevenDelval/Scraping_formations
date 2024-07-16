3c3b4e5850c66a44a1b54125ae1f799029e7fa12
StevenDelval <asteven59220@free.fr>
2024-07-16 16:33:22 +0200

    coment code

--- formation/main.tf
+++ formation/main.tf
@@ -16,13 +16,13 @@ resource "azurerm_postgresql_flexible_server" "postgresql" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
-  administrator_login          = var.admin_user
-  administrator_password       = var.admin_password
-  sku_name                     = "B_Standard_B1ms"
-  storage_mb                   = 32768
-  version                      = "13"
+  administrator_login           = var.admin_user
+  administrator_password        = var.admin_password
+  sku_name                      = "B_Standard_B1ms"
+  storage_mb                    = 32768
+  version                       = "13"
   public_network_access_enabled = true
-  storage_tier                 = "P4"  
+  storage_tier                  = "P4"
 }
 
 resource "azurerm_postgresql_flexible_server_firewall_rule" "firewall_rule" {
@@ -30,48 +30,48 @@ resource "azurerm_postgresql_flexible_server_firewall_rule" "firewall_rule" {
   server_id        = azurerm_postgresql_flexible_server.postgresql.id
   start_ip_address = var.postgres_ip_access_start
   end_ip_address   = var.postgres_ip_access_end
-  
+
 }
 
-resource "azurerm_service_plan" "service_plan" {
-  name                = "example-app-service-plan"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  os_type             = "Linux"
-  sku_name            = "Y1"
+# resource "azurerm_service_plan" "service_plan" {
+#   name                = "example-app-service-plan"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   os_type             = "Linux"
+#   sku_name            = "Y1"
 
-  depends_on = [azurerm_postgresql_flexible_server.postgresql] # Cette ressource sera créée après que le PostgreSQL sera créé.
-}
+#   depends_on = [azurerm_postgresql_flexible_server.postgresql] # Cette ressource sera créée après que le PostgreSQL sera créé.
+# }
 
-resource "azurerm_linux_function_app" "linux_function_app" {
-  name                       = "ScrapyFunction"
-  resource_group_name        = azurerm_resource_group.rg.name
-  location                   = azurerm_resource_group.rg.location
+# resource "azurerm_linux_function_app" "linux_function_app" {
+#   name                = var.function_app_name
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
 
-  storage_account_name       = azurerm_storage_account.storage_account.name
-  storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
-  service_plan_id            = azurerm_service_plan.service_plan.id
+#   storage_account_name       = azurerm_storage_account.storage_account.name
+#   storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
+#   service_plan_id            = azurerm_service_plan.service_plan.id
 
-  site_config {
-    application_stack {
-      python_version = "3.11"
-    }
-  }
-  app_settings = {
-    IS_POSTGRES = var.IS_POSTGRES
-    DB_USERNAME = var.DB_USERNAME
-    DB_HOSTNAME = "${azurerm_postgresql_flexible_server.postgresql.name}.postgres.database.azure.com"
-    DB_PORT = var.DB_PORT
-    DB_NAME = var.DB_NAME
-    DB_PASSWORD = var.DB_PASSWORD
-  }
-  
-}
-resource "null_resource" "publish_function_app" {
-    # c'est la ressource qui permet de publier la fonction sur azure
-  depends_on = [azurerm_function_app.functionapp]
+#   site_config {
+#     application_stack {
+#       python_version = "3.11"
+#     }
+#   }
+#   app_settings = {
+#     IS_POSTGRES = var.IS_POSTGRES
+#     DB_USERNAME = var.DB_USERNAME
+#     DB_HOSTNAME = "${azurerm_postgresql_flexible_server.postgresql.name}.postgres.database.azure.com"
+#     DB_PORT     = var.DB_PORT
+#     DB_NAME     = var.DB_NAME
+#     DB_PASSWORD = var.DB_PASSWORD
+#   }
+# }
 
-  provisioner "local-exec" {
-    command = "func azure functionapp publish ${var.function_app_name} --publish-local-settings"
-  }
-}
+# resource "null_resource" "publish_function_app" {
+#   # c'est la ressource qui permet de publier la fonction sur azure
+#   depends_on = [azurerm_linux_function_app.linux_function_app]
+
+#   provisioner "local-exec" {
+#     command = "func azure functionapp publish ${var.function_app_name} --publish-local-settings -y"
+#   }
+# }
