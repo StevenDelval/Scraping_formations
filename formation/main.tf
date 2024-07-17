3a1a1c9cac9ef39a810c0775ce7b1011eef6d74a
StevenDelval <asteven59220@free.fr>
2024-07-17 16:11:27 +0200

    change sku

--- formation/main.tf
+++ formation/main.tf
@@ -72,7 +72,7 @@ resource "azurerm_service_plan" "service_plan" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "EP1"
+  sku_name            = "Y1"
 
   depends_on = [azurerm_postgresql_flexible_server.postgresql] # Cette ressource sera créée après que le PostgreSQL sera créé.
 }
