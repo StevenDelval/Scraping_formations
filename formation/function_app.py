538e0eb30dbfbe458af7e6b741295b82ff3fc03c
StevenDelval <asteven59220@free.fr>
2024-07-17 09:59:48 +0200

    add schedule with env variable

--- formation/function_app.py
+++ formation/function_app.py
@@ -2,11 +2,13 @@ import logging
 import os
 import subprocess
 import azure.functions as func
-
+from dotenv import load_dotenv
+load_dotenv()
+schedule = os.getenv("schedule")
 app = func.FunctionApp()
 
 
-@app.timer_trigger(schedule="0 10 * * 3", arg_name="myTimer", run_on_startup=False, use_monitor=False)
+@app.timer_trigger(schedule=schedule, arg_name="myTimer", run_on_startup=False, use_monitor=False)
 def timer_trigger1(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
@@ -14,6 +16,8 @@ def timer_trigger1(myTimer: func.TimerRequest) -> None:
     logging.info('Python timer trigger function executed.')
 
     try:
+        # Change de répertoire vers le dossier contenant le projet Scrapy
+        os.chdir("/home/site/wwwroot/formation")
         # Utiliser python -m scrapy pour exécuter le spider
         result = subprocess.run(["scrapy", "crawl", "simplon"], capture_output=True, text=True, check=True)
         result = subprocess.run(["scrapy", "crawl", "francecompetences"], capture_output=True, text=True, check=True)
