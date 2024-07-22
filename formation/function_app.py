76a42a290394b51e3ea8f94330e1bce21b478829
StevenDelval <asteven59220@free.fr>
2024-07-22 11:38:36 +0200

    Change schedule for every 2hours

--- formation/function_app.py
+++ formation/function_app.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 35 */2 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=True)
+@app.timer_trigger(schedule="0 0 */2 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=True)
 def timer_trigger1(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
