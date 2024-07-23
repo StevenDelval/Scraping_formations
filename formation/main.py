8e147df1d0b4651f4d3eb5ae995c8b3cb1624615
StevenDelval <asteven59220@free.fr>
2024-07-23 15:37:58 +0200

    adapte main

--- formation/main.py
+++ formation/main.py
@@ -1,35 +1,26 @@
+import logging
 import os
-from twisted.internet import asyncioreactor
-import asyncio
-if os.name == 'nt':
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-asyncioreactor.install()
-# Définir le réacteur à utiliser
-os.environ['TWISTED_REACTOR'] = 'twisted.internet.asyncioreactor.AsyncioSelectorReactor'
-
-from formation.spiders.simplon import SimplonSpider
-from formation.spiders.francecompetences import FrancecompetencesSpider
-from scrapy.crawler import CrawlerRunner
-from scrapy.utils.project import get_project_settings
-from scrapy.utils.log import configure_logging
-from twisted.internet import reactor, defer
+import subprocess
+from dotenv import load_dotenv
+load_dotenv()
 
 
-settings = get_project_settings()
-configure_logging(settings)
-runner = CrawlerRunner(settings)
+logging.info('Python timer trigger function executed.')
+try:
+    # Change de répertoire vers le dossier contenant le projet Scrapy
+    os.chdir("/home/site/wwwroot/formation")
+    
+    # Utiliser python -m scrapy pour exécuter le spider
+    logging.info('Simplon Spider executed.')
+    simplon_result = subprocess.run(["scrapy", "crawl", "simplon"], capture_output=True, text=True, check=True)
+    logging.info(f"Simplon Spider Output: {simplon_result.stdout}")
+    
+    logging.info('Francecompetences Spider executed.')
+    francecompetences_result = subprocess.run(["scrapy", "crawl", "francecompetences"], capture_output=True, text=True, check=True)
+    logging.info(f"Francecompetences Spider Output: {francecompetences_result.stdout}")
 
-
-@defer.inlineCallbacks
-def crawl():
-    print("SimplonSpider started crawling.")
-    yield runner.crawl(SimplonSpider)
-    print("SimplonSpider finished crawling.")
-    print("FrancecompetencesSpider started crawling.")
-    yield runner.crawl(FrancecompetencesSpider)
-    print("FrancecompetencesSpider finished crawling.")
-    reactor.stop()
-
-
-crawl()
-reactor.run() 
+except subprocess.CalledProcessError as e:
+    logging.error(f"Error executing command: {e.stdout}")
+    logging.error(f"Error executing command: {e.stderr}")
+except Exception as e:
+    logging.error(f"Unexpected error: {str(e)}")
