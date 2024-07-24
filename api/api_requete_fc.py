a9e64836f207b983cbaa48ee1699631596c48146
Martin D <martin.deyres@gmail.com>
2024-07-24 11:09:27 +0200

    finalisation de la factorisation du code

--- api/api_requete_fc.py
+++ api/api_requete_fc.py
@@ -1,7 +1,6 @@
 import requests
 import re
-import csv
-import pandas
+import json
 import psycopg2
 import os
 from sqlalchemy import create_engine, text
@@ -52,11 +51,11 @@ def appel_api(code_certif):
     print(prefixe)
 
     if prefixe == 'rs' :
-        endpoint="code_inventaire%3D"+re.sub(r"\D", "", code_certif) #code RS
+        endpoint="code_inventaire="+re.sub(r"\D", "", code_certif) #code RS
         print(endpoint)
 
     elif prefixe == 'rncp' :
-        endpoint="code_rncp%3D"+re.sub(r"\D", "", code_certif) #code_rncp
+        endpoint="code_rncp="+re.sub(r"\D", "", code_certif) #code_rncp
         print(endpoint)
 
     url = f"{base_url}{endpoint}"
@@ -65,110 +64,33 @@ def appel_api(code_certif):
     response = requests.get(url) #, headers=headers, params=params
     data=response.json()
 
-    print(data)
     return data
 
 
 
 def nettoyage(data):
-    pass
+        # Load JSON data
+  
+
+    # Select specific fields
+    fields_to_keep = ["intitule_certification","intitule_formation", "nom_region", "code_formacode_1", "code_formacode_2","code_formacode_3","code_formacode_4","code_formacode_5"]
+
+    # Extract the selected fields from the results
+    selected_data = []
+    for result in data['results']:
+        selected_result = {field: result[field] for field in fields_to_keep}
+        selected_data.append(selected_result)
+
+    # Print the selected data
+    for item in selected_data:
+        print(item)
+
+    return selected_data
 
 
 
-
-appel_api("rncp36061")
+nettoyage(appel_api("rncp36061"))
 
 
 
 # result_list = fetch_data_from_azure()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# data = pandas.read_csv("data.csv", sep=';', lineterminator ='\n')
-
-#     # Extraire les colonnes spécifiques
-# extracted_data = data[['date_extract', 'nom_departement']]
-# print(extracted_data)
-
-
-
-
-# # Enregistrer les données extraites dans un nouveau fichier CSV
-# extracted_data.to_csv('extracted_data.csv', index=False)
-
-
-#     data = pandas
-# # Vérifier le statut de la réponse
-# if response.status_code == 200:
-
-    # response.content.to_csv('extracted_data.csv', index=False)
-
-
-# # Afficher les données extraites
-# print(extracted_data.head())
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # URL de base de l'API
-# base_url = "https://opendata.caissedesdepots.fr/api.explore.com/v2.1/"
-
-# # Endpoint spécifique de l'API
-# endpoint = "catalog/datasets/moncompteformation_catalogueformation/exports/csv?where=nom_region%3D'Hauts-de-France'"
-# # %20and%20(code_formacode_1%3D'15052'%20or%20code_formacode_2%3D'70322')&limit=20"
-
-# # Votre clé API
-# api_key = "456"
-
-# # Paramètres de la requête (si nécessaire)
-# params = {
-#     # Ajoutez d'autres paramètres selon les besoins
-# }
-
-# # Headers de la requête (si nécessaire)
-# headers = {
-#     "Authorization": f"Bearer {api_key}",
-#     "Content-Type": "application/json",
-# }
-
-
-
-
-
-    # # Connect to an existing database
-    # # with psycopg.connect(f"postgresql://{user}:{password}@{host}:{port}") as conn:
-    # with psycopg.connect(f"sqlite:///database.db") as conn:
-
-    #     # Open a cursor to perform database operations
-    #     with conn.cursor() as cur:
-
-    #         # Execute a command: this creates a new table
-    #         cur.execute("""
-    #            SELECT code_certif FROM france_competences
-    #             """)
-    #         cur.fetchall
