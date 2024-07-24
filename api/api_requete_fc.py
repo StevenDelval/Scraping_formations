f5d613b04788effd0d0c8da078530d515f290a70
Martin D <martin.deyres@gmail.com>
2024-07-24 11:17:38 +0200

    clean du code

--- api/api_requete_fc.py
+++ api/api_requete_fc.py
@@ -16,12 +16,9 @@ def fetch_data_from_azure():
     user=os.getenv("DB_USERNAME")
     database=os.getenv("DB_NAME")
 
-
         # Create the connection URL
-    connection_url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
-    #connection_url = "sqlite:///database.db"
-
-
+    connection_url = f"postgresql://{user}:{password}@{host}:{port}/{database}"     #connection_url = "sqlite:///database.db"
+    
         # Create the SQLAlchemy engine
     engine = create_engine(connection_url, client_encoding='utf-8')
     print(connection_url)
@@ -32,11 +29,10 @@ def fetch_data_from_azure():
         # Create a session
     session = Session()
 
-    # try:
-                # Execute a query
+        # Execute a query
     result = session.execute(text("""SELECT code_certif FROM france_competences"""))
 
-            # Fetch all results from the executed query
+        # Fetch all results from the executed query
     result_list = [row[0] for row in result]
     print(result_list)
     return result_list
@@ -48,7 +44,6 @@ def appel_api(code_certif):
     # Construire l'URL complète et appeler l'api
     base_url = "https://opendata.caissedesdepots.fr/api/explore/v2.1/catalog/datasets/moncompteformation_catalogueformation/records?where="
     prefixe = re.sub(r'\d+', '', code_certif)
-    print(prefixe)
 
     if prefixe == 'rs' :
         endpoint="code_inventaire="+re.sub(r"\D", "", code_certif) #code RS
@@ -69,9 +64,7 @@ def appel_api(code_certif):
 
 
 def nettoyage(data):
-        # Load JSON data
   
-
     # Select specific fields
     fields_to_keep = ["intitule_certification","intitule_formation", "nom_region", "code_formacode_1", "code_formacode_2","code_formacode_3","code_formacode_4","code_formacode_5"]
 
@@ -88,9 +81,10 @@ def nettoyage(data):
     return selected_data
 
 
+if __name__ == "__main__" :
+    
+    tout_les_code_certif_simplon = fetch_data_from_azure()
+    for code in tout_les_code_certif_simplon : 
+        nettoyage(appel_api(code))  #rncp36061
 
-nettoyage(appel_api("rncp36061"))
 
-
-
-# result_list = fetch_data_from_azure()
